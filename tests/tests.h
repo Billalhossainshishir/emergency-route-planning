c6a4ed4eea365f6fca2a284634992bed59c9fa14
Billalhossainshishir <shishir73ahmed@gmail.com>
2026-09-07 00:26:25 +1000

    Add test declarations

---
+++ tests/tests.h
@@ -0,0 +1,11 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+void run_graph_tests(void);
+void run_priority_queue_tests(void);
+void run_dijkstra_tests(void);
+void run_path_reconstruction_tests(void);
+void run_robustness_tests(void);
+void run_all_tests(void);
+
+#endif
